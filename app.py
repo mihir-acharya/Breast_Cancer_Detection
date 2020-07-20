6948782d66f9d015446f6cda1395fd9052f9fca8
Hemant <hemant.a.gautam@accenture.com>
2020-07-20 20:01:53 +0530

    Added file upload option for predicting value

--- app.py
+++ app.py
@@ -1,68 +1,110 @@
 from flask import Flask, jsonify, Response, render_template
-from flask import request,json,escape
+from flask import request, json, escape
 import pickle
 import sklearn
+import csv
+# from werkzeug import secure_filename
+import pandas as pd
 
 # from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 model = pickle.load(open('model/lr_breast_cancer_model.pkl', 'rb'))
 
+
 @app.route("/", methods=['GET'])
 def index():
-   return render_template('index.html')
+    return render_template('index.html')
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+	if request.method == 'POST':  
+		f = request.files['file']  
+		f.save("uploads/" + f.filename)  
+		df = pd.read_csv(f.filename, encoding = 'ISO-8859-1')
+		X = df.drop(columns=['id'], axis = 1)
+		pid = df['id']
+		dfrows = X.shape[0]
+		value_lst = []
+		result_dict = {}
+		for row in range(dfrows):
+		    value_lst.clear()
+		    for value in X.iloc[row]:
+		        value_lst.append(value)
+		    predict_value=model.predict([value_lst])
+		    result_dict[pid[row]] = predict_value[0]
+		print(result_dict)  
+		# return Response("CSV Result Predicted")
+		return render_template("upload_success.html", name = result_dict)  
+
+# @app.route("/file_data_predict", methods=['POST'])
+def file_data_predict():
+	df = pd.read_csv('Breast Cancer-Kaggle.csv',encoding = 'ISO-8859-1')
+	X = df.drop(columns=['id'], axis = 1)
+	pid = df['id']
+	dfrows = X.shape[0]
+	value_lst = []
+	result_dict = {}
+	for row in range(dfrows):
+	    value_lst.clear()
+	    for value in X.iloc[row]:
+	        value_lst.append(value)
+	    predict_value=model.predict([value_lst])
+	    result_dict[pid[row]] = predict_value[0]
+	print(result_dict)  
+	return Response("DataFrame created")
 
 @app.route("/", methods=['POST'])
 def predict():
-	# radius_mean = float(request.form['radius_mean'])
-	# texture_mean = float(request.form['texture_mean'])
-	# perimeter_mean = float(request.form['perimeter_mean'])
-	# area_mean = float(request.form['area_mean'])
-	# smoothness_mean = float(request.form['smoothness_mean'])
-	# compactness_mean = float(request.form['compactness_mean'])
-	# concavity_mean = float(request.form['concavity_mean'])
-	# concave_points_mean = float(request.form['concave_points_mean'])
-	# symmetry_mean = float(request.form['symmetry_mean'])
-	# fractal_dimension_mean = float(request.form['fractal_dimension_mean'])
-	# radius_se = float(request.form['radius_se'])
-	# texture_se = float(request.form['texture_se'])
-	# perimeter_se = float(request.form['perimeter_se'])
-	# area_se = float(request.form['area_se'])
-	# smoothness_se = float(request.form['smoothness_se'])
-	# compactness_se = float(request.form['compactness_se'])
-	# concavity_se = float(request.form['concavity_se'])
-	# concave_points_se = float(request.form['concave_points_se'])
-	# symmetry_se = float(request.form['symmetry_se'])
-	# fractal_dimension_se = float(request.form['fractal_dimension_se'])
-	# radius_worst = float(request.form['radius_worst'])
-	# texture_worst = float(request.form['texture_worst'])
-	# perimeter_worst = float(request.form['perimeter_worst'])
-	# area_worst = float(request.form['area_worst'])
-	# smoothness_worst = float(request.form['smoothness_worst'])
-	# compactness_worst = float(request.form['compactness_worst'])
-	# concavity_worst = float(request.form['concavity_worst'])
-	# concave_points_worst = float(request.form['concave_points_worst'])
-	# symmetry_worst = float(request.form['symmetry_worst'])
-	# fractal_dimension_worst = float(request.form['fractal_dimension_worst'])
+    # radius_mean = float(request.form['radius_mean'])
+    # texture_mean = float(request.form['texture_mean'])
+    # perimeter_mean = float(request.form['perimeter_mean'])
+    # area_mean = float(request.form['area_mean'])
+    # smoothness_mean = float(request.form['smoothness_mean'])
+    # compactness_mean = float(request.form['compactness_mean'])
+    # concavity_mean = float(request.form['concavity_mean'])
+    # concave_points_mean = float(request.form['concave_points_mean'])
+    # symmetry_mean = float(request.form['symmetry_mean'])
+    # fractal_dimension_mean = float(request.form['fractal_dimension_mean'])
+    # radius_se = float(request.form['radius_se'])
+    # texture_se = float(request.form['texture_se'])
+    # perimeter_se = float(request.form['perimeter_se'])
+    # area_se = float(request.form['area_se'])
+    # smoothness_se = float(request.form['smoothness_se'])
+    # compactness_se = float(request.form['compactness_se'])
+    # concavity_se = float(request.form['concavity_se'])
+    # concave_points_se = float(request.form['concave_points_se'])
+    # symmetry_se = float(request.form['symmetry_se'])
+    # fractal_dimension_se = float(request.form['fractal_dimension_se'])
+    # radius_worst = float(request.form['radius_worst'])
+    # texture_worst = float(request.form['texture_worst'])
+    # perimeter_worst = float(request.form['perimeter_worst'])
+    # area_worst = float(request.form['area_worst'])
+    # smoothness_worst = float(request.form['smoothness_worst'])
+    # compactness_worst = float(request.form['compactness_worst'])
+    # concavity_worst = float(request.form['concavity_worst'])
+    # concave_points_worst = float(request.form['concave_points_worst'])
+    # symmetry_worst = float(request.form['symmetry_worst'])
+    # fractal_dimension_worst = float(request.form['fractal_dimension_worst'])
 
-	try: 
-		predicted_value = model.predict([[12.34,22.22,79.85,464.5,0.10120,0.10150,0.05370,0.02822,0.1551,0.06761,0.2949,1.6560,1.955,21.55,0.011340,0.031750,0.03125,0.011350,0.01879,0.005348,13.58,28.68,87.36,553.0,0.1452,0.23380,0.16880,0.08194,0.2268,0.09082]])
-		# predicted_value = model.predict([[15.30,25.27,102.40,732.4,0.10820,0.16970,0.16830,0.08751,0.1926,0.06540,0.4390,1.0120,3.498,43.50,0.005233,0.030570,0.03576,0.010830,0.01768,0.002967,20.27,36.71,149.30,1269.0,0.1641,0.61100,0.63350,0.20240,0.4027,0.09876]])		
-		# predicted_value = model.predict([[radius_mean, texture_mean, perimeter_mean, area_mean, smoothness_mean, compactness_mean, concavity_mean, concave_points_mean, symmetry_mean, fractal_dimension_mean, radius_se, texture_se, perimeter_se, area_se, smoothness_se, compactness_se, concavity_se, concave_points_se, symmetry_se, fractal_dimension_se, radius_worst, texture_worst, perimeter_worst, area_worst, smoothness_worst, compactness_worst, concavity_worst, concave_points_worst, symmetry_worst, fractal_dimension_worst]])
-		if predicted_value[0] == 1:
-			prediction_text = "You Cancer type is Malignant" + str(predicted_value[0])
-		elif predicted_value[0] == 0:
-			prediction_text = "You Cancer type is Benign"
-		else:
-			prediction_text = "No prediction found!"
-		return render_template('index.html', prediction_text="{}".format(prediction_text))	
-	except:
-		return render_template('index.html', prediction_text="Error, please enter all values.")
-
+    try:
+        predicted_value = model.predict([[12.34,22.22,79.85,464.5,0.10120,0.10150,0.05370,0.02822,0.1551,0.06761,0.2949,1.6560,1.955,21.55,0.011340,0.031750,0.03125,0.011350,0.01879,0.005348,13.58,28.68,87.36,553.0,0.1452,0.23380,0.16880,0.08194,0.2268,0.09082]])
+        # predicted_value = model.predict([[15.30, 25.27, 102.40, 732.4, 0.10820, 0.16970, 0.16830, 0.08751, 0.1926, 0.06540, 0.4390, 1.0120, 3.498, 43.50,
+        #                                   0.005233, 0.030570, 0.03576, 0.010830, 0.01768, 0.002967, 20.27, 36.71, 149.30, 1269.0, 0.1641, 0.61100, 0.63350, 0.20240, 0.4027, 0.09876]])
+        # predicted_value = model.predict([[radius_mean, texture_mean, perimeter_mean, area_mean, smoothness_mean, compactness_mean, concavity_mean, concave_points_mean, symmetry_mean, fractal_dimension_mean, radius_se, texture_se, perimeter_se, area_se, smoothness_se, compactness_se, concavity_se, concave_points_se, symmetry_se, fractal_dimension_se, radius_worst, texture_worst, perimeter_worst, area_worst, smoothness_worst, compactness_worst, concavity_worst, concave_points_worst, symmetry_worst, fractal_dimension_worst]])
+        if predicted_value[0] == 1:
+            prediction_text = "You Cancer type is Malignant"
+        elif predicted_value[0] == 0:
+            prediction_text = "You Cancer type is Benign"
+        else:
+            prediction_text = "No prediction found!"
+        return render_template('index.html', prediction_text="{}".format(prediction_text))
+    except:
+        return render_template('index.html', prediction_text="Error, please enter all values.")
 
 
 if __name__ == '__main__':
-   app.run(debug=True)
+    app.run(debug=True)
 
 
 '''
@@ -111,7 +153,7 @@ print(predict_value[0])
 # jdf = open('yourjson.json').read()
 # for x in collection.find({},{ "id": 842302 }):
 # 	print(x)
-# # data = json.loads(jdf)  
+# # data = json.loads(jdf)
 # # collection.insert_one(data)
 
 
@@ -120,10 +162,10 @@ print(predict_value[0])
 # import pandas as pd
 # import sys, getopt, pprint
 # from pymongo import MongoClient
-#CSV to JSON Conversion
+# CSV to JSON Conversion
 # csvfile = open('Breast Cancer-Kaggle.csv', 'r')
 # reader = csv.DictReader( csvfile )
-# mongo_client=MongoClient() 
+# mongo_client=MongoClient()
 # db=mongo_client.october_mug_talk
 # db.segment.drop()
 # header= [ 'id', 'diagnosis', 'radius_mean', 'texture_mean', 'perimeter_mean',
@@ -142,7 +184,6 @@ print(predict_value[0])
 #         row[field]=each[field]
 
 #     collection.insert_one(row)
-
 
 
 ################# Find single Records#####################
